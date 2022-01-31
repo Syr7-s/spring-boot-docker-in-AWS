9df4fc2ef3675b3b9bd7f2bdc26d8ba71a4eb21d
İsa SAYAR <isa.sayar1725@gmail.com>
2022-01-31 20:53:04 +0300

    initial commit

---
+++ src/main/java/com/syrisa/springbootdockerinaws/SpringBootDockerInAwsApplication.java
@@ -0,0 +1,13 @@
+package com.syrisa.springbootdockerinaws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDockerInAwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerInAwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/syrisa/springbootdockerinaws/SpringBootDockerInAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syrisa.springbootdockerinaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerInAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
